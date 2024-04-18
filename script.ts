2ae065769207804dda68b63dd051beb5606483b6
Smustus <143539825+Smustus@users.noreply.github.com>
2024-04-18 18:12:06 +0200

    added code
    added connection template

---
+++ script.ts
@@ -0,0 +1,16 @@
+//Import prisma client
+
+
+
+async function main(){
+  //Add queries here
+}
+
+//Function to log error msg or disconnect whenever our code finishes running
+/* main()
+  .catch(e => {
+    console.log(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  }); */
